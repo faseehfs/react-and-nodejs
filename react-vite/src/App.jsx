f0ff4d0c8a6a953aee20fd489cedc6bb1434c6b6
FaseehFS <faseeh1080@gmail.com>
2025-11-26 12:31:55 +0530

    Create MyButton component

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -1,8 +1,15 @@
+function MyButton() {
+  return (
+    <button>I'm a Button</button>
+  );
+}
+
+
 export default function App() {
   return (
     <>
       <h1>Hello World!</h1>
-      <button>I am not Functional</button>
+      <MyButton />
     </>
   )
 }
