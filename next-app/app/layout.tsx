adcd55dfaa3ab9ece3a60704f6dc60250f999f4a
FaseehFS <faseeh1080@gmail.com>
2025-12-04 22:22:47 +0530

    Add navbar

--- next-app/app/layout.tsx
+++ next-app/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Link from "next/link";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -27,6 +28,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="navbar">
+        <Link href="/">
+          Home
+        </Link>
+        <Link href="/about">
+          About
+        </Link>
+        </div>
         {children}
       </body>
     </html>
