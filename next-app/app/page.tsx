3a46800848585b13082d3a8a124c7bc9b035d478
FaseehFS <faseeh1080@gmail.com>
2025-12-05 22:06:46 +0530

    Write code

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,10 +1,13 @@
 import CountBtn from "@/components/CountBtn";
+// Here, CountBtn is simply the local variable name you choose to assign to its default export.
+// You could rename it like this: import Whatever from "@/components/CountBtn";
 
 export default function Home() {
   return (
     <main>
       <h1>Welcome!</h1>
-      <p>This is a Next.js application.</p>
+      <p>Below is a Button which is stored in the top level 'components' folder. It exists as a file named 'CountBtn.jsx'.</p>
+      <p>'CountBtn.jsx' starts with "use client";, which makes it a client component, so that we can use 'useState'.</p>
       <CountBtn />
     </main>
   );
