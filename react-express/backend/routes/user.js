f6142d5d5a37c88f4dfc33e934b1547b7930e637
FaseehFS <faseeh1080@gmail.com>
2025-12-09 22:18:12 +0530

    Create middleware for user

--- react-express/backend/routes/user.js
+++ react-express/backend/routes/user.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-// router.use() can be used for middleware specific to these routes.
+const userIsAuthenticated = true; // Dummy.
+
+// Middleware for authentcation.
+router.use((req, res, next) => {
+  if (!userIsAuthenticated) {
+    return res.status(401).json({ error: "Not authenticated" });
+  }
+
+  next();
+  // The primary purpose of next() is to pass control to the next middleware function or route handler.
+});
+
+// The routes are protected by the Middleware.
 
 router.get("/login", (req, res) => {
   res.json({
